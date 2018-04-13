76b43220b09734fcc6b1dc3bdeeb89a5de24a2ad
tomparkpro <tom.hyunung.park@gmail.com>
2018-04-14 00:26:16 +0900

    30 Days of Code Challenges
    Solutions for Day 9: Recursion

--- src/pro/tompark/test/hackerrank/day30/_09/Solution.java
+++ src/pro/tompark/test/hackerrank/day30/_09/Solution.java
@@ -17,8 +17,11 @@ public class Solution {
 
     static int factorial(int n) {
         // Complete this function
-
-        return n;
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
     }
 
     public static void main(String[] args) {
