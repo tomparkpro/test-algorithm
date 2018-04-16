7c74d31f258f2ad856dab8f12cf7be43384aa80b
tomparkpro <tom.hyunung.park@gmail.com>
2018-04-16 23:33:19 +0900

    30 Days of Code Challenges
    Solutions for Day 11: 2D Arrays

--- src/pro/tompark/test/hackerrank/day30/_11/Solution.java
+++ src/pro/tompark/test/hackerrank/day30/_11/Solution.java
@@ -15,36 +15,58 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String args[]) {
-//        Scanner in = new Scanner(System.in);
-//        int arr[][] = new int[6][6];
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+//        int arr[][] = {
+//                {1, 1, 1, 0, 0, 0},
+//                {0, 1, 0, 0, 0, 0},
+//                {1, 1, 1, 0, 0, 0},
+//                {0, 0, 2, 4, 4, 0},
+//                {0, 0, 0, 2, 0, 0},
+//                {0, 0, 1, 2, 4, 0}
+//        };
+//        int arr[][] = {
+//                {-1, -1, 0, -9, -2, -2},
+//                {-2, -1, -6, -8, -2, -5},
+//                {-1, -1, -1, -2, -3, -4},
+//                {-1, -9, -2, -4, -4, -5},
+//                {-7, -3, -3, -2, -9, -9},
+//                {-1, -3, -1, -2, -4, -5}
+//        };
+
+
+
 //        for(int i=0; i < 6; i++){
 //            for(int j=0; j < 6; j++){
-//                arr[i][j] = in.nextInt();
+//                System.out.print(arr[i][j]);
 //            }
+//            System.out.println();
 //        }
-
-        int arr[][] = {
-                {1, 1, 1, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0, 0},
-                {0, 0, 2, 4, 4, 0},
-                {0, 0, 0, 2, 0, 0},
-                {0, 0, 1, 2, 4, 0}
-        };
 
         int hgWitdth = 3;           // width of hourglass
         int hgHeight = 3;           // height of hourglass
-        int hgMiddle = 1;           // center from left top corner
         int width = arr[0].length;  // 2D array width
         int height = arr.length;    // 2D array height
 
-
-
-        for(int i=0; i < 6; i++){
-            for(int j=0; j < 6; j++){
-                System.out.print(arr[i][j]);
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i <= (height - hgHeight); i++) {
+            for (int j = 0; j <= (width - hgWitdth); j++) {
+                int top = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                int neck = arr[i+1][j+1];
+                int bottom = arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                int sum = top + neck + bottom;
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
             }
-            System.out.println();
         }
+
+        System.out.println(maxSum);
     }
 }
