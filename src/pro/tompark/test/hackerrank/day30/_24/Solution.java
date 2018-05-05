67e5688114e21b4cb34b9f2b6ddfed02c3ac1446
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-05 23:26:55 +0900

    30 Days of Code Challenges
    Solutions for Day 24: More Linked Lists

---
+++ src/pro/tompark/test/hackerrank/day30/_24/Solution.java
@@ -0,0 +1,68 @@
+package pro.tompark.test.hackerrank.day30._24;
+
+import java.util.Scanner;
+
+/**
+ * Created by TomPark
+ *
+ * @author tom.hyunung.park@gmail.com
+ * github : http://github.com/tomparkpro
+ * <p>
+ * Day 24: More Linked Lists
+ * <p>
+ * https://www.hackerrank.com/challenges/30-linked-list-deletion/problem
+ */
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+
+}
+
+class Solution {
+
+    public static Node removeDuplicates(Node head) {
+        //Write your code here
+        return null;
+    }
+
+    public static Node insert(Node head, int data) {
+        Node p = new Node(data);
+        if (head == null)
+            head = p;
+        else if (head.next == null)
+            head.next = p;
+        else {
+            Node start = head;
+            while (start.next != null)
+                start = start.next;
+            start.next = p;
+
+        }
+        return head;
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while (start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head, ele);
+        }
+        head = removeDuplicates(head);
+        display(head);
+    }
+}
